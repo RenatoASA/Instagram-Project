b94d72ea03f4ee645b92bca9efb65beb9429fa58
Renato <renato_asa@hotmail.com>
2024-07-04 14:02:50 -0400

    commit editar nome

--- src/User.js
+++ src/User.js
@@ -1,10 +1,22 @@
+import React from "react";
+
+
 export default function User() {
+
+
+    const [nome, setNome] = React.useState("");
+
+    function getName(){
+        let namePronpt = prompt("Digite um nome para altera-lo: ");
+        setNome(namePronpt);
+    }
+
     return (<div className="usuario">
         <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
         <div className="texto">
             <span>
-                <strong>catanacomics</strong>
-                <ion-icon name="pencil"></ion-icon>
+                <strong>{!nome ? "catanacomics" : nome}</strong>
+                <ion-icon name="pencil" onClick={getName}></ion-icon>
             </span>
         </div>
     </div>
